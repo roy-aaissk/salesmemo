ff0dc176adf2c14f07e9264efa90ead72723293f
Ryo <7890bask@gmail.com>
2022-02-25 12:21:04 +0900

    :sparkles: loginbutton作成

---
+++ components/beforeLogin/loginButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-btn
+    color="secondary"
+    @click="login"
+    v-if="!$store.state.auth.loggedIn"
+    outlined
+  >
+    Login
+  </v-btn>
+</template>>
+
+<script>
+export default {
+  methods: {
+    login() {
+      console.log('Login attempt')
+      this.$store.dispatch('auth/login')
+    },
+  }
+}
+</script>
