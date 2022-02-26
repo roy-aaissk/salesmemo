4bf6bafa4e9ec102977fb80099ccfaa12db0a943
Ryo <7890bask@gmail.com>
2022-02-26 21:54:25 +0900

    :sparkles: toppage作成

---
+++ pages/top.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <nav class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <Nuxt-link to="/top" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        salesMemo
+      </Nuxt-link>
+    </nav>
+    <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      <login-button/>
+    </nav>
+  </div>
+</template>
+
+<script>
+import LoginButton from '~/components/beforeLogin/loginButton.vue'
+export default {
+  layout: 'welcome',
+  components: {
+    LoginButton,
+  },
+}
+</script>
