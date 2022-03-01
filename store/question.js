19325e667d760f206f6f93a3996cf9391fdc8944
Ryo <7890bask@gmail.com>
2022-03-01 13:49:20 +0900

    :sparkles: 質問追加機能追加不要なコンソールを削除

--- store/question.js
+++ store/question.js
@@ -1,5 +1,5 @@
 import { db } from "~/plugins/firebase.js";
-import { collection, doc, setDoc, Timestamp, getDocs, getDoc, query, where } from "firebase/firestore";
+import { collection, doc, setDoc, Timestamp, getDocs, getDoc, query, where, addDoc } from "firebase/firestore";
 // import { prototype } from "core-js/core/dict";
 
 
@@ -12,22 +12,25 @@ const state = () => ({
 const mutations = {
   getquestion(state, question) {
     state.questions = question
-    console.log(state.question);
   },
   questiondetail(state, question) {
     state.questionsdetail = question
   },
   addquestion(state, question) {
     state.questions.push(question)
+    console.log(question);
+  },
+  reset(state) {
+    Object.assign(state, state())
   },
 }
 
 const actions = {
   async fetchquestion ({commit}) {
     const querySnapshot = await getDocs(collection(db, "question"));
-    let questionlist = [];
     querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
+      let questionlist = [];
+      // console.log(`${doc.id} => ${doc.data()}`);
       console.log(`${doc.id} => ${doc.data().context}`);
       const question = doc.data()
       questionlist.push(
@@ -49,15 +52,27 @@ const actions = {
       console.log("No such document!");
     }
   },
+  async addquestion({commit}, question) {
+    const colRef = collection(db, "question");
+    const data = {
+      title: question.title,
+      context: question.context,
+    };
+    const docRef = await addDoc(colRef, data).then((decRef) =>{
+      console.log(decRef);
+      commit('addquestion',question);
+    });
+    console.log(docRef);
+  }
 }
 
 const getters = {
   getquestion(state){
-    console.log(state.questions)
+    console.log(state.questions);
     return state.questions;
   },
   getquestiondetail(state){
-    console.log(state.questionsdetail)
+    console.log(state.questionsdetail);
     return state.questionsdetail;
   },
   // getanswer(state){
