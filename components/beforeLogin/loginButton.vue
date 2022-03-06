d3a6a98ab0311ffdaaccc54b466a275a8206ba2f
Ryo <7890bask@gmail.com>
2022-03-07 07:03:23 +0900

    :lipstick: googlebutton変更

--- components/beforeLogin/loginButton.vue
+++ components/beforeLogin/loginButton.vue
@@ -5,7 +5,8 @@
     v-if="!$store.state.auth.loggedIn"
     outlined
   >
-    Login
+    <!-- Googleでログイン -->
+    <img src="google/btn_google_signin_dark_pressed_web@2x.png" alt="googleログイン" width="150" height="100">
   </button>
 </template>>
 
